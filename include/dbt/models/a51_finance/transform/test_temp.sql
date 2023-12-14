c877eaf70ea159d85f88f6031561a5f281d202b7
BilalMir135 <bilalmir135@gmail.com>
2023-12-14 21:47:10 +0500

    add a51 and update parsers

---
+++ include/dbt/models/a51_finance/transform/test_temp.sql
@@ -0,0 +1,5 @@
+{{ config(schema="a51_finance", materialized="table") }}
+
+SELECT 
+    {{ var("last_block_timestamp") }} AS last_block_timestamp,
+    {{ var("next_block_timestamp") }} AS next_block_timestamp
